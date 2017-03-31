bf822a47822b5a1f3a9a3b59b557326699f089cf
张松 <zhangsong@zhangsongdeMacBook-Air.local>
2017-03-31 16:31:05 +0800

    修改

---
+++ web编程/10.使用正则表达式匹配注册路由.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+"""
+    通过装饰器来注册路由
+
+"""
+
+from webob import Request, Response
+from webob import exc
+from webob.dec import wsgify
+
+class Application:
+    ROUTER = {}
+
+    @classmethod
+    def register(cls, path):        #定义一个类装饰器， 传入的参数是 environ内的path路径
+        def wrap(handler):          # handler是下面的函数
+            cls.ROUTER[path] = handler      # path为key, handler为value 写入字典
+            #print('{} = > {}'.format(path, handler))    通过打印出来，可查看到，path为ROUTER字典内的key, handler为ROUTER字典内的value
+            return handler
+        return wrap
+
+    @wsgify
+    def __call__(self, request: Request) -> Response:
+        try:
+            return self.ROUTER[request.path](request)
+        except KeyError:
+            raise exc.HTTPNotFound('not found')
+
+@Application.register('/hello')             #装饰器内传入参数 path
+def hello(request: Request) -> Response:
+    name = request.params.get("name", 'anonymous')
+    response = Response()
+    response.text = 'hello {}'.format(name)
+    response.status_code = 200
+    response.content_type = 'text/plain'
+    return response
+
+@Application.register('/')                  #装饰器内传入参数 /
+def index(request: Request) -> Response:
+    return Response(body='hello world', content_type='text/plain')
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+
+    server = make_server('0.0.0.0', 8888, Application())
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.shutdown()
---
+++ web编程/7.动态路由注册.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+from webob import Request, Response   # pip安装webob模块
+from webob.dec import wsgify          #下面需要用到wsgify装饰器
+
+def hello(request: Request) -> Response:    #定义函数hello,
+    name = request.params.get("name", 'anonymous')
+    response = Response()
+    response.text = 'hello {}'.format(name)
+    response.status_code = 200
+    response.content_type = 'text/plain'
+    return response                 #返回response
+
+def index(request: Request) -> Response:    #定义函数 index
+    return Response(body='hello wrold', content_type='text/plain')
+
+router = {          #定义静态路由
+    '/hello': hello,       #如果访问的是 /hello, 则路由到 hello函数，并返回
+    '/': index              # 如果访问的是/（正常访问，不加任何参数）， 则路由到index函数，并返回
+}
+
+@wsgify
+def application(request: Request) -> Response:
+    return router.get(request.path, index)(request)     #返回静态路由访问到的函数
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+
+    server = make_server('0.0.0.0', 8888, application)      #定义bind的IP、端口、
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.shutdown()
+
+
+
+
